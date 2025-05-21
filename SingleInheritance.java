d2564f9c4c033e2be0f834e9f12ee3c5d598be30
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-21 14:09:53 +0530

    Create SingleInheritance.java

---
+++ SingleInheritance.java
@@ -0,0 +1,14 @@
+class SingleInherit {
+    void eat() {
+        System.out.println("animal eats");
+    }
+}
+
+class Animal extends SingleInherit {
+    void drink() {
+        System.out.println("animal drinks water");
+    }
+}
+
+//this is example of single inheritance
+//Note-main function in "Random" class
