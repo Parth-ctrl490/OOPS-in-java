0d57afc1533bbc7d386f59332942334bd02750ab
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-21 16:20:56 +0530

    Create Random2.java

---
+++ Random2.java
@@ -0,0 +1,11 @@
+public class Random2 {
+    public static void main(String[] args)
+    {
+        Animal2 obj1=new Animal2();
+        dog obj2=new dog();
+        puppy obj3=new puppy();
+        obj1.eat();
+        obj2.bark();
+        obj3.weep();
+    }
+}
