c550cdbcea797e6ee48579513b2a56ecd08ec250
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-21 14:10:26 +0530

    Create Random.java

---
+++ Random.java
@@ -0,0 +1,7 @@
+public class Random {
+    public static void main(String[] args) {
+        Animal obj = new Animal();
+        obj.eat();    // inherited from SingleInherit
+        obj.drink();  // defined in Animal
+    }
+}
