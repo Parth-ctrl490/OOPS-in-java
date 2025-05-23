ff9b086fb49e3f7b47dc2ed6d24473d4a9b4ea84
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-23 09:50:29 +0530

    Create Dean.java

---
+++ Dean.java
@@ -0,0 +1,10 @@
+public class Dean {
+    public static void main(String[] args){
+        College clg=new College();
+        coordinator cdr=new coordinator();
+        faculty flt=new faculty();
+        clg.hod();
+        cdr.aiml();
+        flt.automata();
+    }
+}
