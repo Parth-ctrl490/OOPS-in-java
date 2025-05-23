f3da695a579712dbf32dffa0b65ffe8e5b5a2ef6
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-23 10:11:05 +0530

    Create sauraus.java

---
+++ sauraus.java
@@ -0,0 +1,9 @@
+public class sauraus {
+    public static void main(String[] args){
+        Prehistoric pr;
+        pr=new allosaurus();
+        pr.dino();
+        pr=new raptor();
+        pr.dino();
+    }
+}
