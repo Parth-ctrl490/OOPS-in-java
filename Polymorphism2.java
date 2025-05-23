1d5293c220744e1626b2e7a2e3fb22f8bb501833
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-23 10:23:48 +0530

    Update Polymorphism2.java

--- Polymorphism2.java
+++ Polymorphism2.java
@@ -15,3 +15,4 @@ class raptor extends Prehistoric{
     }
 }
 //main function in sauraus class
+//this is example of runtime polymorphism i.e. A subclass provides a specific implementation of a method that is already defined in its superclass. The method to be called is determined at runtime.
