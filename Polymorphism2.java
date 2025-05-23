72afec04eb4f7c57926a17f9356d3cec5fdeadb9
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-23 10:10:30 +0530

    Create Polymorphism2.java

---
+++ Polymorphism2.java
@@ -0,0 +1,17 @@
+//Polymorphism
+class Prehistoric {
+    void dino(){
+        System.out.println("jurassic");
+    }
+}
+class allosaurus extends Prehistoric{
+    void dino(){
+        System.out.println("critaceous");
+    }
+}
+class raptor extends Prehistoric{
+    void dino(){
+        System.out.println("ice age");
+    }
+}
+//main function in sauraus class
