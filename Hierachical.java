498d137973fdf6954dd23a122c4d6d7911779328
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-23 09:49:23 +0530

    Create Hierachical.java

---
+++ Hierachical.java
@@ -0,0 +1,15 @@
+public class College {
+    void hod(){
+        System.out.println("level 3");
+    }
+}
+class coordinator extends College{
+    void aiml(){
+        System.out.println("level 2");
+    }
+}
+class faculty extends College{
+    void automata(){
+        System.out.println("level 1");
+    }
+}
