fd45e53ac937b1b94684095ffc1a088ed73703e6
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-26 15:35:57 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args){
+        Calculator cal=new Calculator();
+        cal.add(23,45);
+        cal.add(23.7,76.7);
+        cal.add(5,8,8);
+    }
+}
