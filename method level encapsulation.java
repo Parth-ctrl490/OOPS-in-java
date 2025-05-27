6c93c4e1ba709221be960152b20c8c90049403b4
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-27 14:23:41 +0530

    Create method level encapsulation.java

---
+++ method level encapsulation.java
@@ -0,0 +1,10 @@
+// 3. Method-Level Encapsulation
+//Methods hide the implementation logic from the 
+// user and only expose input/output.
+class MathOperations {
+    // Method-level encapsulation: logic is hidden inside the method
+    public int calculateSquare(int num) {
+        return num * num;  // caller doesn't see how it's calculated
+    }
+}
+
