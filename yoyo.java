25a5134a10e41e823672b916998656fd7f44c2bc
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-27 14:03:33 +0530

    Create yoyo.java

---
+++ yoyo.java
@@ -0,0 +1,11 @@
+public class yoyo {
+    public static void main(String[] args) {
+        Teacher s = new Teacher();
+
+        s.setName("Parth");
+        s.setAge(20);
+
+        System.out.println(s.getName());  // Parth
+        System.out.println(s.getAge());   // 20
+    }
+}
