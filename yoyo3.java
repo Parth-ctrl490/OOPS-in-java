919e2e06f1ef9e49b7af3e0579adc6d80a4b0e19
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-27 14:18:31 +0530

    Create yoyo3.java

---
+++ yoyo3.java
@@ -0,0 +1,7 @@
+public class yoyo {
+    public static void main(String[] args) {
+        BankAccount s = new BankAccount();
+       s.deposit(954.6);
+        System.out.println(s.getBalance());
+    }
+}
