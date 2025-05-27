d738de421498f60781ac60e7470a78b562407f36
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-05-27 14:26:04 +0530

    Create yoyo4.java

---
+++ yoyo4.java
@@ -0,0 +1,12 @@
+
+
+public class yoyo {
+    public static void main(String[] args) {
+        MathOperations math = new MathOperations();
+
+        int number = 5;
+        int result = math.calculateSquare(number);
+
+        System.out.println("The square of " + number + " is: " + result);
+    }
+}
